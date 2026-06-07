46b26be5b1df0920af14fdf383d18c0cb5c3dbda
space404lab <info.error404lab@gmail.com>
2026-06-07 10:58:37 +0300

    Update cookie-banner.js

--- cookie-banner.js
+++ cookie-banner.js
@@ -25,10 +25,6 @@
     return v ? v.pop() : null;
   }
 
-  function deleteCookie(name) {
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
-  }
-
   /* ─── Яндекс Метрика ───────────────────────────────────────── */
   function loadYandexMetrika() {
     if (window._ymLoaded) return;
